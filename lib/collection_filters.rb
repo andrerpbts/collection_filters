ff055c0238628db612233be2734b71bc1b4f4839
André Rodrigues <andrerpbts@gmail.com>
2019-10-22 15:06:14 -0200

    Add query service. It's the core class to delegate each param to desired filter

--- lib/collection_filters.rb
+++ lib/collection_filters.rb
@@ -1,5 +1,11 @@
-require "collection_filters/engine"
+require 'collection_filters/engine'
+require 'collection_filters/query'
 
 module CollectionFilters
-  # Your code goes here...
+  def self.define(collection:, available_filters: {})
+    Query.new(
+      collection: collection,
+      available_filters: available_filters
+    )
+  end
 end
